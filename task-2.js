205c509d992076e0291a1ee3123da6ca0794f462
serplesniowy <132208333+serplesniowy@users.noreply.github.com>
2025-04-02 17:20:37 +0200

    adding task
    task2

---
+++ task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+    #items;
+
+    constructor(primaryItems) {
+        this.#items = primaryItems;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
